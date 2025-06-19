94f58943f3fa796c81988c721c37dbdb375ccbe7
Shrestha Karmakar <136220039+shre5tha@users.noreply.github.com>
2025-06-19 23:39:55 +0530

    Logger Class

--- Week 1/Design Patterns/SingletonPatternExample/src/Logger.java
+++ Week 1/Design Patterns/SingletonPatternExample/src/Logger.java
@@ -1,43 +1,18 @@
 public class Logger {
-
-    // Private static variable to hold the single instance of the class
     private static Logger instance;
-    
-    // Private constructor to prevent instantiation from outside
     private Logger() {
         System.out.println("Logger instance created");
     }
     
-    /**
-     * Public static method to get the single instance of Logger
-     * @return The single instance of Logger
-     */
     public static Logger getInstance() {
-        // Lazy initialization: create the instance only when it's accessed for the first time
+        // Lazy initialization
         if (instance == null) {
             instance = new Logger();
         }
         return instance;
     }
     
-    /*
-    /* Method to log messages
-     * @param message The message to log
-     */
     public void log(String message) {
         System.out.println("[LOG] " + message);
     }
-    
-    /*
-    // Prevent cloning of the singleton instance
-    @Override
-    protected Object clone() throws CloneNotSupportedException {
-        throw new CloneNotSupportedException();
-    }
-    
-    // Prevent deserialization from creating new instances
-    protected Object readResolve() {
-        return instance;
-    }
-    */
 }
