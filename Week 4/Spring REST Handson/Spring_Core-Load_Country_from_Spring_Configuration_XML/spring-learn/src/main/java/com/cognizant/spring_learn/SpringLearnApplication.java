815102986bcb658bd4b33e533704db756829f769
Shrestha Karmakar <136220039+shre5tha@users.noreply.github.com>
2025-07-14 00:16:44 +0530

    Spring-rest-1-Handson-4 uploaded

---
+++ Week 4/Spring REST Handson/Spring_Core-Load_Country_from_Spring_Configuration_XML/spring-learn/src/main/java/com/cognizant/spring_learn/SpringLearnApplication.java
@@ -0,0 +1,36 @@
+package com.cognizant.spring_learn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+@SpringBootApplication
+public class SpringLearnApplication {
+
+	private static final Logger LOGGER = LoggerFactory.getLogger(SpringLearnApplication.class);
+	
+	public static void displayCountry() {
+		@SuppressWarnings("resource")
+		ApplicationContext context = new ClassPathXmlApplicationContext("country.xml");
+        Country country = context.getBean("country", Country.class);
+        LOGGER.debug("Country : {}", country.toString());
+    }
+
+//	public static void displayCountry() {
+//	    try (ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("country.xml")) {
+//	        Country country = context.getBean("country", Country.class);
+//	        LOGGER.debug("Country : {}", country.toString());
+//	    }
+//	}
+
+	
+	public static void main(String[] args) {
+	    LOGGER.debug("START of main()");
+        displayCountry();
+        LOGGER.debug("END of main()");
+	}
+
+}
